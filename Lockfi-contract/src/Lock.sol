df04f7c9e3e15eea1ee3746e9221f6634402436b
wemmy6673 <okunolawemzy@gmail.com>
2026-01-10 20:19:31 +0100

    adding reentrancy guard to lockfi contract

--- Lockfi-contract/src/Lock.sol
+++ Lockfi-contract/src/Lock.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract Lock is ReentrancyGuard {
     struct Vault {
