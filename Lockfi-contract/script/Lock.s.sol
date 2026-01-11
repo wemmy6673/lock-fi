9289775ce04243e6012cffd18b73a6ee123bcef5
wemmy6673 <okunolawemzy@gmail.com>
2026-01-11 19:44:18 +0100

    lock contract deployed

--- Lockfi-contract/script/Lock.s.sol
+++ Lockfi-contract/script/Lock.s.sol
@@ -9,10 +9,11 @@ contract LockScript is Script {
     function run() external {
 
         address _tokenAddress = 0x9F6fc2403352748E35b7c55fF1b7E2D46927A326;
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         Lock lock;
 
-        vm.startBroadcast();
+        vm.startBroadcast(deployerPrivateKey);
          lock = new Lock(_tokenAddress);
         vm.stopBroadcast();
     }
